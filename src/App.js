86922c5aa223a2f7795fd33585da96e8136f4eb8
amir amgad <155030172+amirhamdy450@users.noreply.github.com>
2024-01-31 17:36:12 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from './Components/Calculator/Calculator.js';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
